9bfb78e0ccc6869460b8ced73ae18bf50ff857a0
shanikaLiu <739539364@qq.com>
2023-06-13 15:16:09 +0800

    Update code to connect db

--- streamlit_sql.py
+++ streamlit_sql.py
@@ -1,9 +1,21 @@
+# import streamlit as st
+# from sqlalchemy import create_engine
+
+
+# engine = create_engine(r'mysql+pymysql://viewuser:)5y)JHhqsFulugU!@master.ccub4ta9krao.ap-southeast-2.rds.amazonaws.com:3306/eiz_test')
+
+# with engine.connect() as connection:
+#     result = connection.execute('select id, companyName, email from accounts where id in (79,277,336,363,4904,911,2333,2677,5906,6682,6832)')
+#     st.dataframe(result,hide_index=True)
+
+
 import streamlit as st
-from sqlalchemy import create_engine
+import timedelta
 
+conn = st.experimental_connection('eiz_test', type='sql')
 
-engine = create_engine(r'mysql+pymysql://viewuser:)5y)JHhqsFulugU!@master.ccub4ta9krao.ap-southeast-2.rds.amazonaws.com:3306/eiz_test')
+# View the connection contents.
+conn
 
-with engine.connect() as connection:
-    result = connection.execute('select id, companyName, email from accounts where id in (79,277,336,363,4904,911,2333,2677,5906,6682,6832)')
-    st.dataframe(result,hide_index=True)
+a = conn.query('select id, companyName, email from accounts where id in (79,277,336,363,4904,911,2333,2677,5906,6682,6832)')
+st.dataframe(a)
