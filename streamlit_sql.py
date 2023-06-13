dfa0b5b6d8ecf59d6d0fcd99475b53e5768a1f33
shanikaLiu <739539364@qq.com>
2023-06-13 14:44:07 +0800

    Update streamlit_sql.py

--- streamlit_sql.py
+++ streamlit_sql.py
@@ -2,8 +2,8 @@ import streamlit as st
 from sqlalchemy import create_engine
 
 
-engine = create_engine('mysql+pymysql://eiz:Adgjl!2345@master.ccub4ta9krao.ap-southeast-2.rds.amazonaws.com:3306/eiz_test')
+engine = create_engine(r'mysql+pymysql://viewuser:)5y)JHhqsFulugU!@master.ccub4ta9krao.ap-southeast-2.rds.amazonaws.com:3306/eiz_test')
 
 with engine.connect() as connection:
-    result = connection.execute('select id, companyName, email from accounts where id in (79,277,336,363,911,2333,2677,5906,6682)')
+    result = connection.execute('select id, companyName, email from accounts where id in (79,277,336,363,4904,911,2333,2677,5906,6682,6832)')
     st.dataframe(result,hide_index=True)
