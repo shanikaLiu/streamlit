f6593dfd473fdcde2129465efe486e7555815721
shanikaLiu <739539364@qq.com>
2023-06-13 17:35:41 +0800

    Update streamlit_sql.py

--- streamlit_sql.py
+++ streamlit_sql.py
@@ -16,6 +16,5 @@ engine = create_engine("mysql+mysqldb://viewuser:)5y)JHhqsFulugU!@master.ccub4ta
 query = 'select id, companyName, email, credit, balance from accounts where id not in (1,284,317,513,911,2333,4832,1978,2677,3430,3793,3980,5474,5906,6572,6484,6681,6682,6684,6728,6734,6735) and credit > 0;'
 
 with engine.connect() as con:
-    stmt = text(query)
-    df = pd.read_sql_query(stmt,con)
+    df = pd.read_sql_query(query,con)
 st.dataframe(df,hide_index=True)
