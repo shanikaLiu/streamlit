44c286e5959e0588b2ce30c82ce7b4b2bae8d50c
shanikaLiu <739539364@qq.com>
2023-06-13 16:05:55 +0800

    Update streamlit_sql.py

--- streamlit_sql.py
+++ streamlit_sql.py
@@ -16,5 +16,5 @@ conn = st.experimental_connection('eiz_test', type='sql')
 # View the connection contents.
 conn
 
-a = conn.query('select id, companyName, email, credit, balance from accounts where id in (79,277,336,363,4904,911,2333,2677,5906,6682,6832);',ttl=600)
-st.dataframe(a)
+a = conn.query('select id, companyName, email, credit, balance from accounts where id not in (1,284,317,513,911,2333,4832,1978,2677,3430,3793,3980,5474,5906,6572,6484,6681,6682,6684,6728,6734,6735) and credit > 0;',ttl=600)
+st.dataframe(a,hide_index=True)
