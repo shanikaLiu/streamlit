76e2f0ea781c595694ebe80470c1290fad0e9709
shanikaLiu <739539364@qq.com>
2023-06-20 15:36:16 +0800

    Update streamlit_sql.py

--- streamlit_sql.py
+++ streamlit_sql.py
@@ -1,8 +1,25 @@
 import pandas as pd
 import streamlit as st
+from sqlalchemy import create_engine, text
 
+db_type = 'mysql+pymysql'
+db_name = 'eiz_test'
+db_user = 'viewuser'
+db_passwd = ')5y)JHhqsFulugU!'
+db_host = 'master.ccub4ta9krao.ap-southeast-2.rds.amazonaws.com'
+db_port = 3306
+
+connect_args = {
+                'connect_timeout': 60,
+                'read_timeout': 90
+                }
+
+engine = create_engine(f'{db_type}://{db_user}:{db_passwd}@{db_host}:{db_port}/{db_name}',connect_args = connect_args)
+
+query = 'select id, email, sales from accounts where id in ( 79, 277, 285, 2333 )'
 # 读取CSV文件
-df = pd.read_csv('./test-files/test.csv')
-
+# df = pd.read_csv('./test-files/test.csv')
+with engine.connect() as con:
+    df = pd.read_sql_query(query,con)
 # 在Streamlit应用程序中展示数据
 st.dataframe(df)
