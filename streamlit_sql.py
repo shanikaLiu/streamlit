381b4688cb7f8fb6951fae75af6c8981b8d30bc7
shanikaLiu <739539364@qq.com>
2023-06-20 10:50:16 +0800

    Update streamlit_sql.py

--- streamlit_sql.py
+++ streamlit_sql.py
@@ -7,7 +7,7 @@ import mysql.connector
 # Uses st.cache_resource to only run once.
 @st.cache_resource
 def init_connection():
-    return mysql.connector.connect(**st.secrets["mysql"])
+    return mysql.connector.connect(**st.secrets.mysql)
 
 conn = init_connection()
 
